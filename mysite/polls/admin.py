39a18c320165e48370ae01404deb0a5092c81fc5
brhamsalih <ibrahim.salih.f9@gmail.com>
2022-09-03 13:40:20 +0300

    final

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 class PollsAdmin(admin.ModelAdmin):
-    list_display = ['question_text','pub_date']
+    #list_display = ['question_text','pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
 admin.site.register(Question, PollsAdmin)
+admin.site.register(Choice)
 
 
 
