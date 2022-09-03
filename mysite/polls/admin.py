385b45004e6d218af5e28e89bc7d539191bba8d1
brhamsalih <ibrahim.salih.f9@gmail.com>
2022-09-03 10:32:23 +0300

    temp,urls,view edit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+class PollsAdmin(admin.ModelAdmin):
+    list_display = ['question_text','pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, PollsAdmin)
+
+
+
+    
